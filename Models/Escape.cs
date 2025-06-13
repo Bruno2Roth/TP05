ae8d06075746de3e8d1bc347ebdbf3aff79c73d5
Maica Trutner <156455994+tiaramencurt@users.noreply.github.com>
2025-06-13 01:34:05 -0300

    Update Escape.cs

--- Models/Escape.cs
+++ Models/Escape.cs
@@ -26,7 +26,7 @@ namespace TP05.Models
         {
             this.respuestas = new Dictionary<int, string>{{1, "m"}, {2, "a"}, {3, "b"}, {4, "c"}, {5, "d"}};
             this.pistas = new Dictionary<int, string>{{1, ""}, {2, ""}, {3, ""}, {4, ""}, {5, ""}};
-            this.secuencias = new string[] {"8", "20", "30", "40", "50"}; //Fibonacci, primos, multiplos de 3, cuadrados, factoriales
+            this.secuencias = new string[] {"3", "1", "2", "12", "5"}; //Fibonacci, primos, multiplos de 3, cuadrados, factoriales
             this.salaActual = 1;
             this.nombreJugador = nJ;
             this.wordle = new Wordle();
